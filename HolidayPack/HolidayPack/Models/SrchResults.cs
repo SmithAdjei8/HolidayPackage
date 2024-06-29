1eb3a9a82fe98fc1e1c048b8fdfd7ea18a58948a
Smith Agyei <SmithAdjei8@gmail.com>
2024-06-29 19:22:45 +0100

    Added the Following Models Flight, Hotel, SrchResults and SrchValues

---
+++ HolidayPack/HolidayPack/Models/SrchResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayPack.Models
+{
+	public class SrchResults
+	{
+		public Flight FlightFound { get; set; }
+		public Hotel HotelFound { get; set; }
+		public decimal TotNightsPrice { get; set; }
+		public decimal TotalPrice { get; set; }
+	}
+}
---
+++ HolidayPack/HolidayPack/Models/SrchValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayPack.Models
+{
+	public class SrchValues
+	{
+		public string DepartingFrom { get; set; }
+		public string TravellingTo { get; set; }
+		public DateTime DepartureDate { get; set; }
+		public int Duration { get; set; }
+	}
+}
