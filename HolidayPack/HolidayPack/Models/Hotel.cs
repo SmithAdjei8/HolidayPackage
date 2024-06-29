e5bf15003f8993c855d3d8b4e6700a1656d5d688
Smith Agyei <SmithAdjei8@gmail.com>
2024-06-29 19:12:35 +0100

    Add Hotel and Flight Models

---
+++ HolidayPack/HolidayPack/Models/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayPack.Models
+{
+	public class Hotel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime Arrival_Date { get; set; }
+		public decimal Price_Per_Night { get; set; }
+		public string Local_Airports { get; set; }
+		public int Nights { get; set; }
+	}
+}
